b7f3117cea22aa058998187fee6c291f33bd0923
henryngo615 <henryngo615@gmail.com>
2025-01-30 09:28:08 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
 
-const API_KEY = "";
+const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 
 function App() {
   const [typing, setTyping] = useState(false); 
