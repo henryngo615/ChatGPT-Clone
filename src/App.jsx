f165eea36fed78d0da785369319be9b58981cb51
henryngo615 <henryngo615@gmail.com>
2025-01-31 13:05:04 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
 
-const API_KEY = process.env.REACT_APP_API;
+const API_KEY = process.env.VITE_SOME_KEY;
 console.log(API_KEY);
 
 
