18e802a39d80b1a27c02f5565c8b08e85f60d3c6
henryngo615 <henryngo615@gmail.com>
2025-01-31 10:18:09 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
 
-const API_KEY = process.env.REACT_APP_NETLIFY_API_KEY;
+const API_KEY = process.env.API_KEY;
 
 function App() {
   const [typing, setTyping] = useState(false); 
